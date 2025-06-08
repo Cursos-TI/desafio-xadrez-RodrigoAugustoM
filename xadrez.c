0696fea4fcfa2003538a30952de0cbd43260c33b
RodrigoAugustoM <202505242582@alunos.estacio.br>
2025-06-08 16:44:04 +0000

    tentando salvar o inicio das alterações

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,59 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 // Rodrigo Augusto Marques
 
+char cima[20] = "Cima\n";
+    char direita[20] = "Direita\n";
+    char esquerda[20] = "Esquerda\n";
+    char baixo[20] = "Baixo\n";
+
+void diagonal(int diresq, int cimbai, int qtdd, char peca[10])
+{
+        printf("Movimento de %s:\n",peca);
+
+    while (qtdd > 0)
+    {
+        // se for pra direita
+        if (diresq == 0)
+        {
+            printf("%s",direita);
+            // se for pra cima
+            if (cimbai == 0)
+            {
+                printf("%s",cima);
+                // diagonal superior direita
+            }
+            // se for pra baixo
+            else if (cimbai == 1)
+            {
+                printf("%s",baixo);
+                // diagonal inferior direita
+            }
+        }
+        // se for pra esquerda
+        else if (diresq == 1)
+        {
+            printf("%s",esquerda);
+            // se for pra cima
+            if (cimbai == 0)
+            {
+                printf("%s",cima);
+                // diagonal superior esquerda
+            }
+            // se for pra baixo
+            else if (cimbai == 1)
+            {
+                printf("%S",baixo);
+                // diagonal inferior esquerda
+            }
+        }
+        qtdd--;
+    }
+}
+
+void vertical(int cimbai,int qtdd) {}
+void horizontal(int esqdir,int qtdd) {}
+
+
 int main()
 {
     // Nível Novato - Movimentação das Peças
@@ -12,23 +65,13 @@ int main()
     int rainhaAnda = 8;
     int bispoAnda = 5;
     int torreAnda = 5;
-    
 
-    char cima[20] = "Cima\n";
-    char direita[20] = "Direita\n";
-    char esquerda[20] = "Esquerda\n";
-    char baixo[20] = "Baixo\n";
     
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("Movimento do bispo:\n");
 
-    for (int iBispo = 0; iBispo < bispoAnda; iBispo++)
-    {
-        
-        printf("%s%s", direita,cima);
-    }
+    diagonal(0, 0, bispoAnda, "bispo");
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
@@ -60,18 +103,15 @@ int main()
     printf("Movimentação do cavalo\n");
     for (int i = 0; i < 1; i++)
     {
-        printf("%s",esquerda); 
+        printf("%s", esquerda);
         i++;
         int j = 0;
         while (j <= 1)
         {
-            printf("%s",baixo);
+            printf("%s", baixo);
             j++;
         }
-           
-    
     }
-    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
